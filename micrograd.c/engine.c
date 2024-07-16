8704295948b51f4103ed445c03e1be65ef000180
Jaward Sesay <jkef96@icloud.com>
2024-07-16 10:27:17 +0800

    with engine.h

--- micrograd.c/engine.c
+++ micrograd.c/engine.c
@@ -1,25 +1,8 @@
-
-#include <stdio.h>
+#include "engine.h"
 #include <stdlib.h>
-#include <math.h>
 #include <string.h>
-
-typedef struct Value Value;
-typedef void (*BackwardFunction)(Value*);
-
-struct Value {
-    double data;
-    double grad;
-    BackwardFunction backward;
-    Value** prev;
-    int prev_count;
-    char* op;
-};
-
-void value_add_backward(Value* v);
-void value_mul_backward(Value* v);
-void value_pow_backward(Value* v);
-void value_relu_backward(Value* v);
+#include <math.h>
+#include <stdio.h>
 
 Value* value_new(double data) {
     Value* v = malloc(sizeof(Value));
@@ -45,7 +28,6 @@ Value* value_add(Value* a, Value* b) {
     out->prev[1] = b;
     out->prev_count = 2;
     out->op = strdup("+");
-    
     out->backward = value_add_backward;
     return out;
 }
@@ -62,7 +44,6 @@ Value* value_mul(Value* a, Value* b) {
     out->prev[1] = b;
     out->prev_count = 2;
     out->op = strdup("*");
-    
     out->backward = value_mul_backward;
     return out;
 }
@@ -80,7 +61,6 @@ Value* value_pow(Value* a, double power) {
     char power_str[20];
     snprintf(power_str, sizeof(power_str), "**%.2f", power);
     out->op = strdup(power_str);
-    
     out->backward = value_pow_backward;
     return out;
 }
@@ -92,18 +72,17 @@ void value_pow_backward(Value* v) {
 }
 
 Value* value_relu(Value* a) {
-    Value* out = value_new(a->data > 0 ? a->data : 0);
+    Value* out = value_new(a->data > 0? a->data : 0);
     out->prev = malloc(sizeof(Value*));
     out->prev[0] = a;
     out->prev_count = 1;
     out->op = strdup("ReLU");
-    
     out->backward = value_relu_backward;
     return out;
 }
 
 void value_relu_backward(Value* v) {
-    v->prev[0]->grad += (v->prev[0]->data > 0) ? v->grad : 0;
+    v->prev[0]->grad += (v->prev[0]->data > 0)? v->grad : 0;
 }
 
 Value* value_neg(Value* a) {
@@ -138,13 +117,13 @@ void build_topo(Value* v, Value*** sorted, int* size, int* capacity) {
 }
 
 void backward(Value* v) {
+    v->grad = 1.0;
     int size = 0;
     int capacity = 10;
     Value** sorted = malloc(capacity * sizeof(Value*));
     
     build_topo(v, &sorted, &size, &capacity);
     
-    v->grad = 1.0;
     for (int i = size - 1; i >= 0; i--) {
         if (sorted[i]->backward) {
             sorted[i]->backward(sorted[i]);
