f6a5a74f5f30eccb05ff685a4829bef41a035d2e
Jaward Sesay <jkef96@icloud.com>
2024-07-16 10:28:48 +0800

    engine headers

---
+++ micrograd.c/engine.h
@@ -0,0 +1,30 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+typedef struct Value {
+    double data;
+    double grad;
+    void (*backward)(struct Value* v);
+    struct Value** prev;
+    int prev_count;
+    char* op;
+} Value;
+typedef void (*BackwardFunction)(Value*);
+
+Value* value_new(double data);
+void value_free(Value* v);
+Value* value_add(Value* a, Value* b);
+void value_add_backward(Value* v);
+Value* value_mul(Value* a, Value* b);
+void value_mul_backward(Value* v);
+Value* value_pow(Value* a, double power);
+void value_pow_backward(Value* v);
+Value* value_relu(Value* a);
+void value_relu_backward(Value* v);
+Value* value_neg(Value* a);
+Value* value_sub(Value* a, Value* b);
+Value* value_div(Value* a, Value* b);
+void build_topo(Value* v, Value*** sorted, int* size, int* capacity);
+void backward(Value* v);
+
+#endif // ENGINE_H
