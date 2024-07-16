126b2ad536ec877de5cf36beeb7bac92d96f28ac
Jaward Sesay <jkef96@icloud.com>
2024-07-16 17:44:47 +0800

    nice

--- micrograd.c/nn.h
+++ micrograd.c/nn.h
@@ -24,8 +24,14 @@ Neuron* neuron_new(int nin, int nonlin);
 Layer* layer_new(int nin, int nout);
 MLP* mlp_new(int nin, int* nouts, int nlayers);
 Value* mlp_call(MLP* m, Value** x);
+Value** mlp_parameters(MLP* m);
+
 void neuron_free(Neuron* n);
 void layer_free(Layer* l);
 void mlp_free(MLP* m);
+
+int mlp_parameters_count(MLP* m);
+void mlp_zero_grad(MLP* m);
+void mlp_update(MLP* m, double learning_rate);
 
 #endif // NN_H
