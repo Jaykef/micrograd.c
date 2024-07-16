1543af5567fffda2c6a2c05f8996429951c7f977
Jaward Sesay <jkef96@icloud.com>
2024-07-16 10:27:45 +0800

    with nn.h

--- micrograd.c/nn.c
+++ micrograd.c/nn.c
@@ -1,24 +1,10 @@
+#include "nn.h"
+#include "engine.h"
 #include <stdlib.h>
-#include <time.h>
-#include "engine.c"
+#include <string.h>
 
-typedef struct {
-    Value** w;
-    Value* b;
-    int nin;
-    int nonlin;
-} Neuron;
-
-typedef struct {
-    Neuron** neurons;
-    int nin;
-    int nout;
-} Layer;
-
-typedef struct {
-    Layer** layers;
-    int nlayers;
-} MLP;
+Value** layer_call(Layer* l, Value** x);
+Value* neuron_call(Neuron* n, Value** x);
 
 Neuron* neuron_new(int nin, int nonlin) {
     Neuron* n = malloc(sizeof(Neuron));
@@ -32,14 +18,6 @@ Neuron* neuron_new(int nin, int nonlin) {
     return n;
 }
 
-Value* neuron_call(Neuron* n, Value** x) {
-    Value* act = n->b;
-    for (int i = 0; i < n->nin; i++) {
-        act = value_add(act, value_mul(n->w[i], x[i]));
-    }
-    return n->nonlin ? value_relu(act) : act;
-}
-
 Layer* layer_new(int nin, int nout) {
     Layer* l = malloc(sizeof(Layer));
     l->neurons = malloc(nout * sizeof(Neuron*));
@@ -49,23 +27,6 @@ Layer* layer_new(int nin, int nout) {
     l->nin = nin;
     l->nout = nout;
     return l;
-}
-
-Value** layer_call(Layer* l, Value** x) {
-    Value** out = malloc(l->nout * sizeof(Value*));
-    if (out == NULL) {
-        printf("Error: Failed to allocate memory for layer output\n");
-        return NULL;
-    }
-    for (int i = 0; i < l->nout; i++) {
-        out[i] = neuron_call(l->neurons[i], x);
-        if (out[i] == NULL) {
-            printf("Error: neuron_call returned NULL for neuron %d\n", i);
-            free(out);
-            return NULL;
-        }
-    }
-    return out;
 }
 
 MLP* mlp_new(int nin, int* nouts, int nlayers) {
@@ -86,41 +47,47 @@ Value* mlp_call(MLP* m, Value** x) {
     Value** out = x;
     for (int i = 0; i < m->nlayers; i++) {
         out = layer_call(m->layers[i], out);
-        if (out == NULL) {
-            printf("Error: layer_call returned NULL for layer %d\n", i);
-            return NULL;
-        }
     }
-    return out[0]; 
+    return out[0];
+}
+
+Value** layer_call(Layer* l, Value** x) {
+    Value** out = malloc(l->nout * sizeof(Value*));
+    for (int i = 0; i < l->nout; i++) {
+        out[i] = neuron_call(l->neurons[i], x);
+    }
+    return out;
+}
+
+Value* neuron_call(Neuron* n, Value** x) {
+    Value* act = n->b;
+    for (int i = 0; i < n->nin; i++) {
+        act = value_add(act, value_mul(n->w[i], x[i]));
+    }
+    return n->nonlin? value_relu(act) : act;
 }
 
 void neuron_free(Neuron* n) {
-    if (n) {
-        for (int i = 0; i < n->nin; i++) {
-            value_free(n->w[i]);
-        }
-        free(n->w);
-        value_free(n->b);
-        free(n);
+    for (int i = 0; i < n->nin; i++) {
+        value_free(n->w[i]);
     }
+    free(n->w);
+    value_free(n->b);
+    free(n);
 }
 
 void layer_free(Layer* l) {
-    if (l) {
-        for (int i = 0; i < l->nout; i++) {
-            neuron_free(l->neurons[i]);
-        }
-        free(l->neurons);
-        free(l);
+    for (int i = 0; i < l->nout; i++) {
+        neuron_free(l->neurons[i]);
     }
+    free(l->neurons);
+    free(l);
 }
 
 void mlp_free(MLP* m) {
-    if (m) {
-        for (int i = 0; i < m->nlayers; i++) {
-            layer_free(m->layers[i]);
-        }
-        free(m->layers);
-        free(m);
+    for (int i = 0; i < m->nlayers; i++) {
+        layer_free(m->layers[i]);
     }
+    free(m->layers);
+    free(m);
 }
